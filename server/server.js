00ef6960724d297118f93f48e448a9fb63d26fc4
priambodop <priambodop>
2018-08-10 10:24:21 +0700

    Added utils folder which consist of generateMessage moduls

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname + '/../public')
 const port = process.env.PORT || 3000
@@ -13,28 +14,17 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected...');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app!',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user join',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user was joined.'));
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
-      from : message.from,
-      text : message.text,
-      createdAt : new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected...');
+    
   });
 });
 
