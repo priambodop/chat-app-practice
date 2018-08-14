2b84b8a00cea258dc2c873afc10244a70c710915
priambodop <priambodop>
2018-08-14 10:22:15 +0700

    Make a form for create a messages through the browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,15 +6,23 @@ socket.on('connect', function(){
 
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', function(){
   console.log('Disconnected from the server');
 });
 
-socket.emit('createMessage', {
-  from: 'John',
-  text: 'Im in'
-}, function(data){
-  console.log('Got it, ', data);
+jQuery('#message-form').on('submit', function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(data){
+    console.log(data);
+  });
 });
