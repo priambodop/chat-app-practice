afa4bdc34d75495a15a4fe031fd96f47d24659bd
priambodop <priambodop>
2018-08-15 17:13:55 +0700

    Able to store a user list to the class and add updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,17 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to the server');
+
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function(err){
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    }else{
+      console.log('No error :)');
+    }
+  });
 });
 
 socket.on('newMessage', function(message){
@@ -10,6 +21,17 @@ socket.on('newMessage', function(message){
   li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
+});
+
+socket.on('updateUserList', function(users){
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+
 });
 
 socket.on('disconnect', function(){
